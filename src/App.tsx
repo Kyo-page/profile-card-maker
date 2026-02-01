630c635e4174da646915b1a9d79e1b8830490e77
Kyoka <yuzz025@gmail.com>
2026-02-01 19:00:30 +0900

    tailwindのclass名が効かない問題を修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import { useState } from 'react';
-import { PreviewCard } from './components/PreviewCard';
-import { CardEditor } from './components/CardEditor';
+import { useState } from "react";
+import { PreviewCard } from "./components/PreviewCard";
+import { CardEditor } from "./components/CardEditor";
 
-export type CardColor = 'amber' | 'lime' | 'teal';
+export type CardColor = "amber" | "lime" | "teal";
 
 function App() {
-  const [name, setName] = useState('John Smith');
-  const [color, setColor] = useState<CardColor>('amber');
+    const [name, setName] = useState("John Smith");
+    const [color, setColor] = useState<CardColor>("amber");
 
-  return (
-    <>
-      <PreviewCard name={name} color={color} />
-      <CardEditor
-        name={name}
-        setName={setName}
-        color={color}
-        setColor={setColor}
-      />
-    </>
-  );
+    return (
+        <>
+            <PreviewCard name={name} color={color} />
+            <CardEditor name={name} setName={setName} color={color} setColor={setColor} />
+        </>
+    );
 }
 
 export default App;
--- src/components/PreviewCard.tsx
+++ src/components/PreviewCard.tsx
@@ -1,20 +1,24 @@
-import type { CardColor } from '../App';
+import type { CardColor } from "../App";
+
+const bgColors = {
+    amber: "bg-amber-300",
+    lime: "bg-lime-300",
+    teal: "bg-teal-300",
+};
 
 type PreviewProps = {
-  name: string;
-  color: CardColor;
+    name: string;
+    color: CardColor;
 };
 
 export const PreviewCard = ({ name, color }: PreviewProps) => {
-  return (
-    <>
-      <div className={`card shadow-sm bg-${color}-300`}>
-        <div className="card-body">
-          <div className="text-center text-lg font-bold tracking-wide">
-            {name}
-          </div>
-        </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className={`card shadow-sm ${bgColors[color]}`}>
+                <div className="card-body">
+                    <div className="text-center text-lg font-bold tracking-wide">{name}</div>
+                </div>
+            </div>
+        </>
+    );
 };
